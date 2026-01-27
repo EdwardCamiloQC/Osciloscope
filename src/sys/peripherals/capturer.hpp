5ca11c25d9d761212db67414e67c475982a4c774
EdwardCamiloQC <edward.camilo.q@gmail.com>
2026-01-27 16:22:13 -0500

    Delete the asociations with the voltages and add the dependency with the voltages in the readValues function.

--- src/sys/peripherals/capturer.hpp
+++ src/sys/peripherals/capturer.hpp
@@ -6,6 +6,9 @@
 #ifndef _CAPTURER_H_
     #define _CAPTURER_H_
 
+    //Forward declaration
+    class VoltageSignal;
+
     /** \brief It is a interface that allows change between some catchers. 
         \class
      */
@@ -16,9 +19,15 @@
             virtual ~Capturer() = default;
 
             /** \brief Reads the values that it arrives.
+                \param volt1: Reference to the voltage one.
+                \param volt2: Reference to the voltage two.
+                \param volt3: Reference to the voltage three.
+                \param volt4: Reference to the voltage four.
                 \param nValues: Number of data to read.
              */
-            virtual void readValues(unsigned int nValues) = 0;
+            virtual void readValues(VoltageSignal *volt1, VoltageSignal *volt2,
+                                    VoltageSignal *volt3, VoltageSignal *volt4,
+                                    unsigned int nValues) = 0;
 
             /** \brief Sets the sampling rate to acquire the data for the external hardware.
                 \param freq: Frequency.
