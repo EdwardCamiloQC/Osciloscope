7a2454927aaffa533f08ff31ff0c59612d5dbd1c
EdwardCamiloQC <edward.camilo.q@gmail.com>
2026-03-03 15:22:50 -0500

    The static attributes were defined. And the length to shift the voltages is lenSignals/32 = 4.

--- src/sys/oscilloscope.cpp
+++ src/sys/oscilloscope.cpp
@@ -3,39 +3,46 @@
 #include <sys/oscilloscope.hpp>
 #include <sys/peripherals/puertoSerie.hpp>
 
-//----------
-//----------
-//      STATICS ATTRIBUTES
-//----------
-//----------
+//==================================================
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// STATICS ATTRIBUTES
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//==================================================
 Oscilloscope* Oscilloscope::instance_ = nullptr;
 std::mutex Oscilloscope::mutex_;
+std::atomic<bool> Oscilloscope::stateOnOff_ {false};
+std::atomic<bool> Oscilloscope::stateStartStop_ {false};
 
-//----------
-//----------
-//      METHODS
-//----------
-//----------
+//==================================================
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// PRIVATE METHODS
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//==================================================
 Oscilloscope::Oscilloscope(unsigned int lenSignals = 128):
-    stateOnOff_(true),
-    stateStartStop_(false),
-    voltage1(lenSignals, SIGNAL_COLOR::RED),
-    voltage2(lenSignals, SIGNAL_COLOR::ORANGE),
-    voltage3(lenSignals, SIGNAL_COLOR::BLUE),
-    voltage4(lenSignals, SIGNAL_COLOR::GREEN),
-    screen(), signalCapturer(nullptr){
-        voltage1.calculateSpectrum();
-        voltage2.calculateSpectrum();
-        voltage3.calculateSpectrum();
-        voltage4.calculateSpectrum();
-        signalCapturer.selectCapturer(std::make_unique<ComSerial>());
-        signalCapturer.setSampleFrequency(50);
-        signalCapturer.setMultiplierFrequency(1);
+    voltage1_(lenSignals, SIGNAL_COLOR::RED),
+    voltage2_(lenSignals, SIGNAL_COLOR::ORANGE),
+    voltage3_(lenSignals, SIGNAL_COLOR::BLUE),
+    voltage4_(lenSignals, SIGNAL_COLOR::GREEN),
+    screen_(), signalCapturer_(nullptr)
+{
+    stateOnOff_.store(true, std::memory_order_release);
+    voltage1_.calculateSpectrum();
+    voltage2_.calculateSpectrum();
+    voltage3_.calculateSpectrum();
+    voltage4_.calculateSpectrum();
+    signalCapturer_.selectCapturer(std::make_unique<ComSerial>());
+    signalCapturer_.setSampleFrequency(50);
+    signalCapturer_.setMultiplierFrequency(1);
 }
 
 Oscilloscope::~Oscilloscope(){
 }
 
+//==================================================
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// PUBLIC METHODS
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//==================================================
 Oscilloscope* Oscilloscope::getInstance(){
     std::lock_guard<std::mutex> lock(mutex_);
     if(instance_ == nullptr){
@@ -54,7 +61,6 @@ void Oscilloscope::destroyInstance(){
 }
 
 int Oscilloscope::run(int &argc, char**& argv){
-    signalCapturer.start(static_cast<unsigned int>(voltage1.length/8));
-    return screen.show(argc, argv);
+    signalCapturer_.start(static_cast<unsigned int>(voltage1_.length/32));
+    return screen_.show(argc, argv);
 }
-
