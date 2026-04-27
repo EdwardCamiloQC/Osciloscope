4846022ec2fca36713a9785cd4a632aef8adcf76
EdwardCamiloQC <edward.camilo.q@gmail.com>
2026-04-27 16:30:26 -0500

    Commented code removed and chrono lib too.

--- src/application/signalCapturer.cpp
+++ src/application/signalCapturer.cpp
@@ -1,5 +1,3 @@
-#include <chrono>
-
 #include <application/signalCapturer.hpp>
 #include <adapters/capturer.hpp>
 #include <application/oscilloscope.hpp>
@@ -37,29 +35,9 @@ void SignalCapturer::init(void *userData){
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //==================================================
 void SignalCapturer::catch_voltages([[maybe_unused]]void *userData){
-    OSCILLOSCOPE::Oscilloscope *osc = OSCILLOSCOPE::Oscilloscope::get_instance();
-    /*using clock = std::chrono::steady_clock;
-    using msD = std::chrono::duration<double, std::milli>;
-
-    double timeDiv = timeDiv_.load(std::memory_order_acquire);
-    const double timeWindow = static_cast<double>(8) * static_cast<double>(timeDiv);
-    const double timeCapture = (4.0 * static_cast<double>(M_VALUES_TO_SHIFT) * timeWindow) / (static_cast<double>(LENGTH_VOLT_SIGNAL));
-
-    const clock::duration period = std::chrono::duration_cast<clock::duration>(msD{timeCapture});
-
-    auto nextDeadLine = clock::now();*/
+    auto *osc = OSCILLOSCOPE::Oscilloscope::get_instance();
 
     while(osc->stateOnOff_.load(std::memory_order_acquire)){
         capturer_->catch_data(osc);
-
-        /*nextDeadLine += period;
-        const auto now = clock::now();
-        if(now < nextDeadLine){
-            std::this_thread::sleep_until(nextDeadLine);
-        }else{
-            do{
-                nextDeadLine += period;
-            }while(nextDeadLine <= now);
-        }*/
     }
 }
