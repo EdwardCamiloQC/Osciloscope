538902190c18ae273bb413392774768ac0aff730
EdwardCamiloQC <edward.camilo.q@gmail.com>
2026-06-15 01:36:35 -0500

    It generates the document but doesn't apply any format.

--- src/frameworksAndDrivers/persistence/systemFile/docGenerator.cpp
+++ src/frameworksAndDrivers/persistence/systemFile/docGenerator.cpp
@@ -1,4 +1,11 @@
+#include <fstream>
+#include <string.h>
+#include <assert.h>
+#include <mutex>
 #include "frameworksAndDrivers/persistence/systemFile/docGenerator.hpp"
+#include "domain/signals/voltageSignal.hpp"
+#include "infrastructure/adapters/screen.hpp"
+#include "infrastructure/adapters/signalCapturer.hpp"
 
 using namespace DRV_FRAMW;
 
@@ -7,10 +14,60 @@ using namespace DRV_FRAMW;
 // PUBLIC METHODS
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //==================================================
-void DocGenerator::associate_voltages([[maybe_unused]]DOMN::VoltageSignal* volts){
-    //
+void DocGenerator::associate_voltages(DOMN::VoltageSignal* volts){
+    voltsPtr_ = volts;
 }
 
-void DocGenerator::generate_doc([[maybe_unused]]const char *directory, [[maybe_unused]]APP::TYPE_DOC type, [[maybe_unused]]float timeDiv) const{
-    //
+void DocGenerator::associate_screen(APP::IScreen* screenPtr){
+    screenPtr_ = screenPtr;
+}
+
+void DocGenerator::associate_signal_capturer(APP::ISignalCapturer* sigCapPtr){
+    signalCapPtr_ = sigCapPtr;
+}
+
+void DocGenerator::generate_doc(const char *directory, APP::TYPE_DOC type, float timeDiv) const{
+    assert(screenPtr_ != nullptr);
+    assert(signalCapPtr_ != nullptr);
+
+    std::fstream archivo;
+    std::string ruta = directory;
+
+    switch(type){
+        case APP::TYPE_DOC::CSV:
+            ruta.append("voltajes.csv");
+            break;
+        case APP::TYPE_DOC::JSON:
+            ruta.append("voltajes.json");
+            break;
+        case APP::TYPE_DOC::TSV:
+            ruta.append("voltajes.tsv");
+            break;
+        case APP::TYPE_DOC::TXT:
+            ruta.append("voltajes.txt");
+            break;
+    }
+
+    archivo.open(ruta, std::ios_base::out);
+
+    if(archivo.is_open()){
+        archivo << "fs = " << timeDiv << "Hz\n\n";
+
+        for(int s = 0; s < 4; s++){
+            archivo << "Señal: " << s << std::endl;
+            for(size_t i = 0; i < voltsPtr_[0].ringBuffer_.get_lenght(); i++){
+                {
+                    std::lock_guard<std::mutex> lock(signalCapPtr_->get_mutex());
+                    archivo << *voltsPtr_[s].ringBuffer_.get_ring_buffer_ref(i) << "V\n";
+                }
+            }
+            archivo.write("\n",1);
+        }
+        screenPtr_->set_message("Archivo Generado.\n", 4);
+    }else{
+        perror("Error");
+        return;
+    }
+
+    archivo.close();
 }
--- src/frameworksAndDrivers/persistence/systemFile/docGenerator.hpp
+++ src/frameworksAndDrivers/persistence/systemFile/docGenerator.hpp
@@ -1,11 +1,10 @@
 /** \file docGenerator.hpp
  *  \author Edward Camilo
  *  \version v1.0
- *  \date May 2026
+ *  \date June 2026
  */
 #pragma once
 
-#include <string>
 #include "application/IDocGenerator.hpp"
 
 /** \namespace DRV_FRAMW.
@@ -18,25 +17,40 @@ namespace DRV_FRAMW{
         // METHODS
         //====================
         public:
-            /**
-             * 
+            /** \note Constructor by default.
              */
             DocGenerator() = default;
 
-            /**
-             * 
+            /** \note Destructor by default.
              */
             ~DocGenerator() = default;
 
-            /**
-             * 
+            /** \brief Reference the voltages.
+             *  \param voltsPtr: Voltages.
              */
             void associate_voltages(DOMN::VoltageSignal* voltsPtr) override final;
+
+            /** \brief Reference the screen.
+             *  \param screenPtr: Screen.
+             */
+            void associate_screen(APP::IScreen* screenPtr) override final;
+
+            /** \brief Reference the signal capturer.
+             *  \param screenPtr: Signal Capturer.
+             */
+            void associate_signal_capturer(APP::ISignalCapturer* sigCapPtr) override final;
 
             /** \brief Saves the voltage signals.
              *  \param directory: Route path to the directory.
              *  \param type: Type of the file to save.
              */
             void generate_doc(const char *directory, APP::TYPE_DOC type, float timeDiv) const override final;
+        //====================
+        // ATTRIBUTES
+        //====================
+        private:
+            DOMN::VoltageSignal*  voltsPtr_     {nullptr};
+            APP::IScreen*         screenPtr_    {nullptr};
+            APP::ISignalCapturer* signalCapPtr_ {nullptr};
     };
 }
